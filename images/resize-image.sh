7ed3833c32cb0047c26801be3eaa1ca6be30f4da
The Zen Van <hey@thezenvan.com>
2017-05-22 13:07:03 -0400

    fixed issue with JPG images not displaying

--- images/resize-image.sh
+++ images/resize-image.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# rename JPG to jpg so github displated correctly
+for f in IMG*.JPG;
+do
+	mv "$f" "${f%.JPG}.jpg"
+	echo "Rename $f"
+done
+
 # find a better way to see what files were last added to the images folder
 for i in $(find . -type f -name "IMG*" -ctime -1 -print0 | xargs -0 echo);
 do
-	echo $i;
+	echo $i
 	# resize images
 	if [[ "$OSTYPE" == "linux-gnu" ]]; then
 		convert -strip -quality 85% -resize 1280 $i $i
